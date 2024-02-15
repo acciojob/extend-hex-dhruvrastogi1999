255ca3ad70270f148664a7b0ab4484839eec1183
dhruvrastogi1999 <139809107+dhruvrastogi1999@users.noreply.github.com>
2024-02-15 22:58:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 const extendHex = (shortHex) => {
   // write your code here
+	let i = shortHex.length-3, isCapital=false;
+	let output = "#";
+	while(i<shortHex.length){
+		let assciCode= shortHex[i].charCodeAt(0);
+		if(assciCode>=65 && assciCode<=90){
+			isCapital = true;
+		}
+		output += shortHex[i] + shortHex[i];
+		i++;
+	}
+	return isCapital ? output.toUpperCase() : output;
 };
 
 // Do not change the code below.
-const shortHex = prompt("Enter Short Hex.");
-alert(extendHex(shortHex));
+// const shortHex = prompt("Enter Short Hex.");
+// alert(extendHex(shortHex));
